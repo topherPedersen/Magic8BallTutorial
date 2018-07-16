6c68cb2085cd762ed32acffe44addf20d0404739
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2018-07-16 10:02:58 -0500

    Update magic8ball.py

--- magic8ball.py
+++ magic8ball.py
@@ -1,5 +1,13 @@
 # https://bit.ly/2KZqTo8
+
+import random
+
 print("Welcome to Magic 8 Ball")
 # print("What is your question?")
 userInput = raw_input("What is your question?")
-print(userInput)
+
+randomNumber = random.randint(1, 2)
+if randomNumber == 1:
+  print("Yes")
+else:
+  print("No")
